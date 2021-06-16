f42eabd2ad8236bbc36b6a0bb7e1cd8ab970a425
dionjwa1 <dionjwa@gmail.com>
2021-06-15 21:17:10 -0700

    lab2

---
+++ src/error-handlers/505.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = (err, req, res, next) => {
+    console.log(error);
+    res.status(500).send(err);
+    
+}
---
+++ src/server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+// const logger = require('./middleware/logger.js');
+// const validator = require('./middlewares/validator.js');
+
+const app = express();
+module.exports = app;
